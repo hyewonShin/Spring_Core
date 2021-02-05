181517a80d236b30bd5284ea753968f7c223b028
hyewonShin <rusy7225@naver.com>
2021-02-05 16:54:58 +0900

    Initial commit

---
+++ Spring_core-master/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,36 @@
+package hello.core.member;
+
+import hello.core.Appconfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        Appconfig appconfig = new Appconfig();
+        memberService = appconfig.memberService();
+    }
+
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L,"memaberA",Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1l);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+
+
+
+    }
+
+}
